2b207f9763d3c8bd78ca5f29ed1b4dfcf3c82815
rainNightZ <zye9709@163.com>
2023-02-13 16:16:10 +0800

    feature: 增加md文档编辑器

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import VueMarkdownEditor from "@kangc/v-md-editor";
+import VMdPreview from '@kangc/v-md-editor/lib/preview';
+import '@kangc/v-md-editor/lib/style/preview.css';
+import "@kangc/v-md-editor/lib/style/base-editor.css";
+import githubTheme from '@kangc/v-md-editor/lib/theme/github.js';
+import hljs from 'highlight.js';
+import vuepressTheme from "@kangc/v-md-editor/lib/theme/vuepress.js";
+import "@kangc/v-md-editor/lib/theme/style/vuepress.css";
+import Prism from "prismjs";
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+VueMarkdownEditor.use(vuepressTheme, {
+  Prism,
+});
+VMdPreview.use(githubTheme, {
+  Hljs: hljs,
+});
 
 const app = createApp(App)
+app.use(VueMarkdownEditor);
+app.use(VMdPreview);
 
 app.use(createPinia())
 app.use(router)
